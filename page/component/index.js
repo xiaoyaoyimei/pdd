cf178c4886fbf1511d1eb4b46caf4de0a119f252
xiaoyaoyimei <1206902591@qq.com>
2018-08-10 18:02:11 +0800

    pdd小程序

---
+++ page/component/index.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    imgUrls: [
+      '/image/p1.png',
+      '/image/p2.png',
+      '/image/p3.png'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 800,
+  }
+})
