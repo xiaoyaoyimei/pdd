5de3e23b5fc974047ad5f8116e813f1fb3ee2613
xiaoyaoyimei <1206902591@qq.com>
2018-09-05 17:22:46 +0800

    pdd

--- page/component/index/index.js
+++ page/component/index/index.js
@@ -45,4 +45,37 @@ Page({
       path: '/page/component/index/index' // 分享路径
     }
   },
+  free(){
+    wx.showModal({
+      title: '免费服务',
+      content: '加微信号xiaoyaoyimei0717咨询',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })  
+  },
+  testpay(){
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: 'https://api.weixin.qq.com/sns/jscode2session?appid="wxc5ebc3772314e2ca"&secret="ece09911ade7f1cfc1ae58c6c7854fd7"&js_code=JSCODE&grant_type=authorization_code',
+            data: {
+              code: res.code
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    });
+
+
+    
+  }
 })
